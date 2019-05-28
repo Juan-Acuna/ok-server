8327140a3b2e03fa9dbb0056dcbf0463ffa496ca
Juan <unusuario256@hotmail.com>
2019-05-28 12:23:08 -0400

    Controller

---
+++ Servidor.OKCasa/Controllers/UsuarioController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Dapper;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Servidor.OKCasa.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Usuario")]
+    public class UsuarioController : Controller
+    {
+        //GET
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+        //POST
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+        //PUT
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+        //DELETE
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
