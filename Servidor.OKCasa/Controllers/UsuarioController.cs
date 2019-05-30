c9136b1f9e1a9509b86647a5895c21702ce9b325
Juan Acuña <unusuario256@hotmail.com>
2019-05-30 14:59:34 -0400

    Datos

--- Servidor.OKCasa/Controllers/UsuarioController.cs
+++ Servidor.OKCasa/Controllers/UsuarioController.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using Dapper;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Servidor.OKCasa.Controllers
