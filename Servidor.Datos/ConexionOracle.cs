00e1d582a8105b829a4d53b8cd20aaeaf96b0242
Juan Acuña <unusuario256@hotmail.com>
2019-05-30 15:48:55 -0400

    ConexionOracle en proceso

--- Servidor.Datos/ConexionOracle.cs
+++ Servidor.Datos/ConexionOracle.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using Dapper;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,37 @@ namespace Servidor.Datos
 {
     class ConexionOracle
     {
+        private const String SOURCE = "";
+        private const String USER   = "";
+        private const String PASSWD = "";
+        private const String STRING_CONEXION = "DATA SOURCE="+SOURCE+";USER ID="+USER+";PASSWORD="+PASSWD+";";
+        private static OracleConnection con = new OracleConnection(STRING_CONEXION);
+        private static ConexionOracle _instance = new ConexionOracle();
+        public static ConexionOracle Conexion
+        {
+            get {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+                else
+                {
+                    return _instance = new ConexionOracle();
+                }
+            }
+        }
+        public static List<T> GetAll<T>()
+        {
+            return con.GetAll<List<T>>();
+        }
+        public static T Get<T>(int id)
+        {
+            return con.Get<T>(id);
+        }
+        public static T Get<T>(String rut)
+        {
+            String sql = "SELECT FROM USUARIO WHERE rut='"+rut+"';";
+            return con.QueryFirstOrDefault<T>(sql);
+        }
     }
 }
